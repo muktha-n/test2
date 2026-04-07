98f70b7dcab9ef8915900d7953ace06ff7c23f5e
SHISHIR <SHISHIR@LAPTOP-KF48MFC5>
2026-04-07 14:08:36 +0530

    string6

---
+++ tesr2/src/tesr2/tesr15.java
@@ -0,0 +1,18 @@
+package tesr2;
+//Given a string, return true if the string starts with "hi" and false otherwise.
+//startHi("hi there") → true
+//startHi("hi") → true
+//startHi("hello hi") → false
+
+public class tesr15 {
+	public boolean startHi(String str) {
+		  if(str.length()>=2){
+		   return (str.substring(0,2).equals("hi"));
+		  }
+		  else{
+		    return false;
+		  }
+		}
+
+
+}
