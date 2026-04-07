68dc00d54b735b19c4bb77580d8b1d33cfcb396d
SHISHIR <SHISHIR@LAPTOP-KF48MFC5>
2026-04-07 14:12:40 +0530

    10 to20

---
+++ tesr2/src/tesr2/test17.java
@@ -0,0 +1,22 @@
+package tesr2;
+
+//Given 2 int values, return true if either of them is in the range 10..20 inclusive.
+//in1020(12, 99) → true
+//in1020(21, 12) → true
+//in1020(8, 99) → false
+
+public class test17 {
+	public boolean in1020(int a, int b) {
+		  if(a>=10&&a<=20){
+		    return true;
+		  }
+		  else if(b>=10&&b<=20){
+		    return true;
+		  }
+		  else{
+		    return false;
+		  }
+		}
+
+
+}
