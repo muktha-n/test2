710eb3b32a48f51e1e88a36731fefcfefc0acbe9
SHISHIR <SHISHIR@LAPTOP-KF48MFC5>
2026-04-07 13:35:12 +0530

    sum double

---
+++ tesr2/src/tesr2/test4.java
@@ -0,0 +1,20 @@
+package tesr2;
+//Given two int values, return their sum. Unless the two values are the same, then return double their sum.
+
+
+//sumDouble(1, 2) → 3
+//sumDouble(3, 2) → 5
+//sumDouble(2, 2) → 8
+
+public class test4 {
+	public int sumDouble(int a, int b) {
+		  if(a==b){
+		    return 2*(a+b);
+		  }
+		  else {
+		    return a+b;
+		  }
+		}
+
+
+}
