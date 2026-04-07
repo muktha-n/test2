0701ded6ab1772c3807e2d956e3b7ab50549ee51
SHISHIR <SHISHIR@LAPTOP-KF48MFC5>
2026-04-07 13:38:46 +0530

    diff

---
+++ tesr2/src/tesr2/test5.java
@@ -0,0 +1,18 @@
+package tesr2;
+//Given an int n, return the absolute difference between n and 21, except return double the absolute difference if n is over 21.
+//diff21(19) → 2
+//diff21(10) → 11
+//diff21(21) → 0
+
+public class test5 {
+	public int diff21(int n) {
+		  if(n>=21){
+		    return 2*( n-21);
+		  }
+		  else{
+		    return 21-n;
+		  }
+		}
+
+
+}
